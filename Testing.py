4bd935f5be1991db34f1f46240ed33a551c7e5db
Ivan <ivansharankov3@gmail.com>
2019-09-24 15:27:44 +0200

    Added sample code i use to work with

--- Testing.py
+++ Testing.py
@@ -19,3 +19,40 @@ final_dictionary = json.loads(ini_string)
 print("final dictionary", str(final_dictionary))
 print("type of final_dictionary", type(final_dictionary))
 print(final_dictionary['nikhil'])
+
+
+
+# import json
+# # with open('user.json','w') as file:
+# #     json.dump({
+# #         "name": "Foo Bar",
+# #         "age": 78,
+# #         "friends": ["Jane","John"],
+# #         "balance": 345.80,
+# #         "other_names":("Doe","Joe"),
+# #         "active":True,
+# #         "spouse":None
+# #     }, file, sort_keys=True, indent=4)
+#
+#
+# humanData = json.loads(json.dumps({
+#     'people': [
+#         {
+#             'Name': 'Ivan Sharankov',
+#             'Age': 21,
+#             'Height': 190,
+#             'Weight': 78.5,
+#             'Active': True,
+#             'Balance': 100_000,
+#         },
+#         {
+#             'Name': 'Alex Mechev',
+#             'Age': 30,
+#             'Height': 181,
+#             'Weight': 70.5,
+#             'Active': False,
+#             'Balance': 1000
+#         }
+#     ]
+# }, indent=2))
+#
