7065a39e175e36005f392511e9d527cd5ccb6a7a
Ivan <ivansharankov3@gmail.com>
2019-10-19 00:09:20 +0200

    Template Multicast servers

--- Subreflector_Server/sender.py
+++ Subreflector_Server/sender.py
@@ -9,4 +9,5 @@ while True:
     time.sleep(1)
     multicast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     multicast_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-    multicast_sock.sendto(status_message, (MULTICAST, MULTIPORT))
+    timee = str(time.time()).encode()
+    multicast_sock.sendto(timee, (MULTICAST, MULTIPORT))
