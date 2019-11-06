c6e52c80e1566ff958ec0088b93bc9d657293fc3
Ivan <ivansharankov3@gmail.com>
2019-11-06 17:03:55 +0100

    random files updated

--- Protocol_Practice/UDP Suit/UDP_Basic_Client.py
+++ Protocol_Practice/UDP Suit/UDP_Basic_Client.py
@@ -3,13 +3,13 @@ import time
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     destination_address = ('', ***REMOVED***)
-    sock.settimeout(5)
+    sock.settimeout(2)
 
     while True:
         time.sleep(0.001)
         data = input("What to send: ")
 
-        # Connect to server and send data
+        # Connect to server and send dataEFFELSBERG:MTSUBREFLECTOR:RESETCONNECTION
         sock.sendto(str.encode(data), destination_address)
         print("Sent:     {}".format(data))
 
