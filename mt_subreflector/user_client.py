d3c27a6cf32a5c7d79da98a34ddda4e9e9aae64c
Ivan <ivansharankov3@gmail.com>
2019-11-12 18:13:44 +0100

    Copy of UDP_Basic_Client.py with increased usability

---
+++ mt_subreflector/user_client.py
@@ -0,0 +1,73 @@
+import socket
+import time
+
+def recv_msg(sock):
+    # Receive data from the server
+    try:
+        received = sock.recv(***REMOVED***)
+        print("Received: {}".format(received.decode('utf-8')))
+    except socket.timeout:
+        print("Socket timed out, Try again")
+
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.settimeout(4)
+
+    address, port = None, None
+    while port is None:
+        try:
+            address = input("Specify the address (empty or zero for local): ")
+            port = int(input("Specify port (default for SR_program: ***REMOVED***): "))
+            assert port > ***REMOVED*** and port < 65535
+
+            destination_address = (address, port)
+            sock.sendto(str.encode("\n"), destination_address)
+            recv_msg(sock)
+
+        except AssertionError:
+            port = None
+            print("port should be between ***REMOVED*** and 65535, "
+                  "lower values reserved (except 0)")
+
+        except OSError as e:
+            port = None
+            print(f"OSError occured: {e}")
+
+    while True:
+        time.sleep(0.1)
+        data = input("What to send: ")
+
+        # Connect to server and send data
+        sock.sendto(str.encode(data), destination_address)
+        print("Sent:     {}".format(data))
+
+        recv_msg(sock)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
