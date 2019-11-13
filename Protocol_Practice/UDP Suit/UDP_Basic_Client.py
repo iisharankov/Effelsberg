48ab5591e31c89ba3fd1d129747cfcef0f197639
Ivan <ivansharankov3@gmail.com>
2019-11-13 21:39:59 +0100

    Documentation update

--- Protocol_Practice/UDP Suit/UDP_Basic_Client.py
+++ Protocol_Practice/UDP Suit/UDP_Basic_Client.py
@@ -1,9 +1,22 @@
 import socket
 import time
 
+def recv_msg(sock):
+    # Receive data from the server
+    try:
+        received = sock.recv(***REMOVED***)
+        print("Received: {}".format(received.decode('utf-8')))
+    except socket.timeout:
+        print("Socket timed out, Try again")
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    destination_address = ('', ***REMOVED***)
+    address = input("Specify the address (empty for local): ")
+    port = int(input("Specify port (***REMOVED*** is default for SR_program): "))
+    destination_address = (address, port)
     sock.settimeout(4)
+
+    sock.sendto(str.encode("\n"), destination_address)
+    recv_msg(sock)
 
     while True:
         time.sleep(0.001)
@@ -13,12 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.sendto(str.encode(data), destination_address)
         print("Sent:     {}".format(data))
 
-        # Receive data from the server
-        try:
-            received = sock.recv(***REMOVED***)
-            print("Received: {}".format(received.decode('utf-8')))
-        except socket.timeout:
-            print("Socket timed out, Try again")
+        recv_msg(sock)
 
 
 
