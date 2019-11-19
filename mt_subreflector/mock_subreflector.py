b6117f9c48320392dd2f483eb4254227b17fd868
Ivan <ivansharankov3@gmail.com>
2019-11-19 23:26:33 +0100

    Added functionality to detect which ctype struct is received in receive port

--- mt_subreflector/mock_subreflector.py
+++ mt_subreflector/mock_subreflector.py
@@ -3,11 +3,9 @@ import ctypes
 import socket
 import pickle
 import logging
-import datetime
 import threading
 
 from mt_subreflector import subreflector_client
-import mtcommand
 
 SUBREF_ADDR = ***REMOVED***
 SUBREF_READ_PORT = ***REMOVED***
@@ -17,7 +15,7 @@ buffer_size = ***REMOVED***
 # TODO: ADD port ***REMOVED*** to receive commands, copy client.py methods to decode
 
 # Opens and creates mirror samples of what the subreflector sends
-#TODO: Local location, FIX before release
+# TODO: Local location, FIX before release
 location = "/home/ivan/PycharmProjects/Effelsberg/mt_subreflector/data/" \
            "Pickled_Subreflector_Output.p"
 with open(location, "rb") as pickle_instance:
@@ -27,49 +25,85 @@ with open(location, "rb") as pickle_instance:
 
 
 def main():
-    T = threading.Thread(target=sender, args=(), name="***REMOVED*** Port")
-    T.daemon = False
-    T.start()
+    t = threading.Thread(target=sender, args=(), name="***REMOVED*** Port")
+    t.daemon = False
+    t.start()
 
-    T2 = threading.Thread(target=receiver, args=(), name="***REMOVED*** Port")
-    T2.daemon = False
-    T2.start()
+    t2 = threading.Thread(target=Receiver().create_socket, args=(), name="***REMOVED*** Port")
+    t2.daemon = False
+    t2.start()
 
 
-def receiver():
-    # create TCP/IP socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        server_address = (SUBREF_ADDR, SUBREF_WRITE_PORT)
-        print('starting up on %s port %s' % server_address)
+class Receiver:
 
-        # Sets the maximum buffersize of the socket to ***REMOVED***
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buffer_size)
+    def __init__(self):
+        self.unpacked_data = 6
 
-        # SO_REUSEADDR tells kernel to use socket even if in TIME_WAIT state
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(server_address)
-        sock.listen(2)
+    @staticmethod
+    def unpack(ctype, buf):
+        """
+        Opposite of pack. Takes a bytes string and creates a ctypes structure
+        instance given the appropriate structure to model from
+        :param ctype:  Ctype structure class for reference
+        :param buf: bytes string to unpack to class instance
+        :return: instance of a ctypes structure
+        """
+        cstring = ctypes.create_string_buffer(buf)
+        ctype_instance = ctypes.cast(ctypes.pointer(cstring),
+                                     ctypes.POINTER(ctype)).contents
+        return ctype_instance
 
-        while True:
-            time.sleep(1)
-            # listen for incoming connections (server mode)
-            # with one connection a time
-            connection, client_address = sock.accept()
+    def create_socket(self):
+        # create TCP/IP socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            server_address = (SUBREF_ADDR, SUBREF_WRITE_PORT)
+            print('starting up on %s port %s' % server_address)
 
-            # receive the data in small chunks and print it
+            # Sets the maximum buffersize of the socket to ***REMOVED***
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buffer_size)
+
+            # SO_REUSEADDR tells kernel to use socket even if in TIME_WAIT state
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind(server_address)
+            sock.listen(2)
+
             while True:
                 time.sleep(1)
+                # listen for incoming connections (server mode)
+                connection, client_address = sock.accept()
 
-                pass
-                # data = connection.recv(buffer_size)
-                # mt = mtcommand.MTCommand(True)
-                #
-                # unpacked_data = mt.unpack(mt.interlock_command_to_struct(), data)
-                #
-                # print(unpacked_data)
+                # intercept SR command to deconstruct
+                while True:
+                    time.sleep(1)
+                    data = connection.recv(buffer_size)
+                    if data:
+                        self.find_structure_type(data)
+
+    def find_structure_type(self, data):
+        # This method unpacks the data message with a
+        # BasicStructure class that can extract the command type
+
+        try:
+            _basic_unpack = self.unpack(BasicStructure, data)
+            assert _basic_unpack.command in [100, 101, 102, 106]
+
+        except AttributeError or AssertionError:
+            pass
+        else:
+            if _basic_unpack.command == 100:
+                self.unpacked_data = self.unpack(AsfStructure, data)
+            elif _basic_unpack.command == 101:
+                self.unpacked_data = self.unpack(HexapodStructure, data)
+            elif _basic_unpack.command == 102:
+                self.unpacked_data = self.unpack(PolarStructure, data)
+            elif _basic_unpack.command == 106:
+                self.unpacked_data = self.unpack(InterlockStructure, data)
 
 
 
+
+
+# Part that emulates port ***REMOVED*** and spits out data packets
 def sender():
     # create TCP/IP socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -99,7 +133,7 @@ def sender():
                     print("Initialization request received")
 
                     while True:
-                        time.sleep(0.001)
+                        time.sleep(0.01)
                         # Loop sending messages just like the real SR does
                         try:
                             connection.send(second_msg)
@@ -111,6 +145,91 @@ def sender():
                             break
 
                 break
+
+
+# # # # # # # # STRUCTURE CLASSES # # # # # # # #
+class InterlockStructure(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("start_flag", ctypes.c_uint32),
+                ("message_length", ctypes.c_int32),
+                ("command_serial_number", ctypes.c_int32),
+                ("command", ctypes.c_int16),
+                ("mode", ctypes.c_int16),
+                ("elevation", ctypes.c_double),
+                ("reserved", ctypes.c_double),
+                ("end_flag", ctypes.c_uint32)]
+
+
+class AsfStructure(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("start_flag", ctypes.c_uint32),
+                ("message_length", ctypes.c_int32),
+                ("command_serial_number", ctypes.c_int32),
+                ("command", ctypes.c_int16),
+                ("mode", ctypes.c_int16),
+                ("offset_dr_nr", ctypes.c_int16),
+                ("offset_active", ctypes.c_uint16),
+                ("offset_value1", ctypes.c_int16),
+                ("offset_value2", ctypes.c_int16),
+                ("offset_value3", ctypes.c_int16),
+                ("offset_value4", ctypes.c_int16),
+                ("offset_value5", ctypes.c_int16),
+                ("offset_value6", ctypes.c_int16),
+                ("offset_value7", ctypes.c_int16),
+                ("offset_value8", ctypes.c_int16),
+                ("offset_value9", ctypes.c_int16),
+                ("offset_value10", ctypes.c_int16),
+                ("offset_value11", ctypes.c_int16),
+                ("end_flag", ctypes.c_uint32)]
+
+
+class HexapodStructure(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("start_flag", ctypes.c_uint32),
+                ("message_length", ctypes.c_int32),
+                ("command_serial_number", ctypes.c_int32),
+                ("command", ctypes.c_int16),
+                ("fashion", ctypes.c_int16),
+                ("mode_lin", ctypes.c_int16),
+                ("anzahl_lin", ctypes.c_uint16),
+                ("phase_lin", ctypes.c_double),
+                ("p_xlin", ctypes.c_double),
+                ("p_ylin", ctypes.c_double),
+                ("p_zlin", ctypes.c_double),
+                ("v_lin", ctypes.c_double),
+                ("mode_rot", ctypes.c_int16),
+                ("anzahl_rot", ctypes.c_uint16),
+                ("phase_rot", ctypes.c_double),
+                ("p_xrot", ctypes.c_double),
+                ("p_yrot", ctypes.c_double),
+                ("p_zrot", ctypes.c_double),
+                ("v_rot", ctypes.c_double),
+                ("end_flag", ctypes.c_uint32)]  # DWORD
+
+
+class PolarStructure(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("start_flag", ctypes.c_uint32),
+                ("message_length", ctypes.c_int32),
+                ("command_serial_number", ctypes.c_int32),
+                ("command", ctypes.c_int16),
+                ("mode", ctypes.c_int16),
+                ("p_soll", ctypes.c_double),
+                ("v_cmd", ctypes.c_double),
+                ("end_flag", ctypes.c_uint32)]
+
+
+class BasicStructure(ctypes.Structure):
+    """
+    BasicStructure is used to get the command value of the structure as it
+    is the identifier that tells us what real Structure class to use. This
+    structure only unpacks the first 4 values, the rest are lost.
+    """
+    _pack_ = 1
+    _fields_ = [("start_flag", ctypes.c_uint32),
+                ("message_length", ctypes.c_int32),
+                ("command_serial_number", ctypes.c_int32),
+                ("command", ctypes.c_int16)]
 
 
 if __name__ == '__main__':
