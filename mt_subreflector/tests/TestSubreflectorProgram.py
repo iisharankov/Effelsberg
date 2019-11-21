614fbd204e817f0a879f8591d9d1af31e30857dd
Ivan <ivansharankov3@gmail.com>
2019-11-21 23:41:25 +0100

    issues working with testing import

--- mt_subreflector/tests/TestSubreflectorProgram.py
+++ mt_subreflector/tests/TestSubreflectorProgram.py
@@ -9,7 +9,7 @@ import json
 import os
 import sys, os
 
-from mt_subreflector  import mock_subreflector
+from mt_subreflector import mock_subreflector, subreflector_program
 
 SUBREF_ADDR = ***REMOVED***
 SUBREF_READ_PORT = ***REMOVED***
@@ -30,7 +30,7 @@ class StartupMockSubreflector:
 
     def __init__(self):
         self.thread = None
-        self.stop_threads = False
+
         self.receiver = mock_subreflector.Receiver()
 
     def start_sr_client(self):
@@ -45,9 +45,8 @@ class StartupMockSubreflector:
             self.thread = threading.Thread(
                 target=self.run_mock_sr, args=(), name='moch_sr_module')
 
-            self.thread.daemon = False  # Demonize thread
+            self.thread.daemon = True  # Demonize thread
             logging.debug(f"Threading set to {self.thread.daemon}.")
-            time.sleep(0.5)
             self.thread.start()
             logging.debug(f"Thread started successfully with "
                           f"thread ID: {self.thread.ident}")
@@ -63,21 +62,75 @@ class StartupMockSubreflector:
         """
         self.receiver.create_socket()
 
+class StartupReflectorProgram:
+    """
+    Simple class that correctly starts up the mock_subreflector.py script
+    in a thread be tested
+    """
+
+    def __init__(self):
+        self.thread = None
+
+    def start_program(self):
+        """
+        This method sets up a non-daemon thread to initiate the
+        subreflector_client.py in the background. It calls the next method which
+        is what actually instantiates SubreflectorClient
+        """
+
+        try:
+            logging.debug(f"Creating Thread")
+            self.thread = threading.Thread(
+                target=self.run_sr_program, args=(), name='moch_sr_module')
+
+            self.thread.daemon = True  # Demonize thread
+            logging.debug(f"Threading set to {self.thread.daemon}.")
+            self.thread.start()
+            logging.debug(f"Thread started successfully with "
+                          f"thread ID: {self.thread.ident}")
+            # self.close_sr_client()
+        except Exception as Er:
+            print(Er)
+            logging.exception("Exception starting StartupMockSubreflector")
+
+    def run_sr_program(self):
+        """
+        creates instance of SubreflectorClient which starts listening to the
+        subreflector.
+        """
+        subreflector_program.start_connections(True)
+
+
+
+sr_inst = StartupReflectorProgram()
+sr_inst.start_program()
 
 mock_sr = StartupMockSubreflector()
 mock_sr.start_sr_client()
 
-class TestCanTest:
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(5)
+destination_address = ('', ***REMOVED***)
 
+class TestCanTest():
 
     def test_can_add(self):
         four = 4
         assert four == 4
 
+    def test_can_is_one(self):
+        assert 1 == 1
+
+    def test_can_send_to_mt(self):
+        data = "fail"
+
+        assert data == 'fail'
+
+
     def test_can_reach(self):
         assert mock_sr
         real = mock_sr.receiver.unpacked_data
-        assert real == 6
+        assert real == 5
 
 # class TestSubreflectorProgram:
 #     def test_server_starts_tcp_server(self):
