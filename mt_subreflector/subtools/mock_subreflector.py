ce83060fb5c6da98ffc17f6ad18cdf6af7d3d887
Ivan <ivansharankov3@gmail.com>
2019-12-03 16:44:14 +0100

    fixed Receiver class

--- mt_subreflector/subtools/mock_subreflector.py
+++ mt_subreflector/subtools/mock_subreflector.py
@@ -29,7 +29,8 @@ def start_mock_server(my_receiver):
     t.daemon = False
     t.start()
 
-    t2 = threading.Thread(target=my_receiver.create_socket, args=(),
+    my_receiver.create_socket()
+    t2 = threading.Thread(target=my_receiver.get_message, args=(),
                           name="***REMOVED*** Port")
     t2.daemon = False
     t2.start()
@@ -38,20 +39,15 @@ def start_mock_server(my_receiver):
 class Receiver:
 
     def __init__(self):
-        # self.unpacked_data = 61  # TODO method does not replace this???
-        self.do_run = True
-
+        self.lock = threading.Lock()
+        self.connection = None
 
     def shutdown(self):
-        print('Receiver shutdown')
-        print(f"The do_run value is {self.do_run}")
-        templock = threading.Lock()
 
-
-        with templock:
-            self.do_run = False
-        print(f"The do_run value is {self.do_run}")
-
+        with self.lock:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.sock.close()
+            print("Socket shutdown")
 
     @staticmethod
     def unpack(ctype, buf):
@@ -68,40 +64,38 @@ class Receiver:
         return ctype_instance
 
     def create_socket(self):
-        # create TCP/IP socket
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            server_address = (config.LOCAL_IP, config.SR_WRITE_PORT)
-            # sock.settimeout(3)
-            sock.setblocking(False)
-            # Sets the maximum buffersize of the socket to ***REMOVED***
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, config.BUFFER_SIZE)
 
-            # SO_REUSEADDR tells kernel to use socket even if in TIME_WAIT state
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(server_address)
-
-            sock.listen(2)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (config.LOCAL_IP, config.SR_WRITE_PORT)
+        self.sock.settimeout(3)
 
 
-            while True:
+        # SO_REUSEADDR tells kernel to use socket even if in TIME_WAIT state
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, config.BUFFER_SIZE)
 
-                # listen for incoming connections (server mode)
-                if not self.do_run:
-                    print("BREAKING")
-                    break
-                try:
-                    connection, client_address = sock.accept()
-                except BlockingIOError:
-                    pass
-                else:
-                    # intercept SR command to deconstruct
-                    while True:
-                        try:
-                            data = connection.recv(config.BUFFER_SIZE)
-                            if data:
-                                self.find_structure_type(data)
-                        except ConnectionError:
-                            break
+        self.sock.bind(server_address)
+        self.sock.listen(2)
+
+
+    def get_message(self):
+
+        try:
+            assert self.sock
+
+            # Connecction is made on first test, but kept at module level
+            if not self.connection:
+                self.connection, self.client_address = self.sock.accept()
+
+            data = self.connection.recv(config.BUFFER_SIZE)
+            self.find_structure_type(data)
+
+
+        except BlockingIOError or ConnectionError:
+            pass
+
+        except AssertionError as E:
+            raise E
 
     def find_structure_type(self, data):
         # This method unpacks the data message with a
@@ -111,85 +105,65 @@ class Receiver:
             _basic_unpack = self.unpack(process_message.BasicStructure, data)
             assert _basic_unpack.command in [100, 101, 102, 106]
 
-        except AttributeError or AssertionError:
-            pass
+        except AttributeError or AssertionError as E:
+            print(f"WARNING: an exception occurred while unpacking. {E}")
+            raise E
+
         else:
 
             if _basic_unpack.command == 100:
                 unpacked_data = self.unpack(process_message.AsfStructure, data)
-                self.asf_parser(unpacked_data)
+
+                assert unpacked_data.command == 100
+                self.asf_mode = unpacked_data.mode
+                self.asf_offset_dr_nr = unpacked_data.offset_dr_nr
+                self.asf_offset_active = unpacked_data.offset_active
+                self.asf_offset_value1 = unpacked_data.offset_value1
+                self.asf_offset_value2 = unpacked_data.offset_value2
+                self.asf_offset_value3 = unpacked_data.offset_value3
+                self.asf_offset_value4 = unpacked_data.offset_value4
+                self.asf_offset_value5 = unpacked_data.offset_value5
+                self.asf_offset_value6 = unpacked_data.offset_value6
+                self.asf_offset_value7 = unpacked_data.offset_value7
+                self.asf_offset_value8 = unpacked_data.offset_value8
+                self.asf_offset_value9 = unpacked_data.offset_value9
+                self.asf_offset_value10 = unpacked_data.offset_value10
+                self.asf_offset_value11 = unpacked_data.offset_value11
+
             elif _basic_unpack.command == 101:
                 unpacked_data = self.unpack(process_message.HexapodStructure, data)
-                self.hexapod_parser(unpacked_data)
+
+                assert unpacked_data.command == 101
+                self.hexapod_mode_lin = unpacked_data.mode_lin
+                self.hexapod_anzahl_lin = unpacked_data.anzahl_lin
+                self.hexapod_phase_lin = unpacked_data.phase_lin
+                self.hexapod_p_xlin = unpacked_data.p_xlin
+                self.hexapod_p_ylin = unpacked_data.p_ylin
+                self.hexapod_p_zlin = unpacked_data.p_zlin
+                self.hexapod_p_zlin = unpacked_data.p_zlin
+                self.hexapod_v_lin = unpacked_data.v_lin
+                self.hexapod_mode_rot = unpacked_data.mode_rot
+                self.hexapod_anzahl_rot = unpacked_data.anzahl_rot
+                self.hexapod_phase_rot = unpacked_data.phase_rot
+                self.hexapod_p_xrot = unpacked_data.p_xrot
+                self.hexapod_p_yrot = unpacked_data.p_yrot
+                self.hexapod_p_zrot = unpacked_data.p_zrot
+                self.hexapod_v_rot = unpacked_data.v_rot
+
             elif _basic_unpack.command == 102:
                 unpacked_data = self.unpack(process_message.PolarStructure, data)
-                self.polar_parser(unpacked_data)
+
+                assert unpacked_data.command == 102
+                self.interlock_mode = unpacked_data.mode
+                self.interlock_p_soll = unpacked_data.p_soll
+                self.interlock_v_cmd = unpacked_data.v_cmd
+
             elif _basic_unpack.command == 106:
                 self.unpacked_data = self.unpack(process_message.InterlockStructure, data)
-                # print(unpacked_data.elevation)
-                # print("jnvfkjvkjfvjsfvjJNJNEVJINVIEJNVIPJJRF")
-                # print(unpacked_data)
-                # self.dataa = unpacked_data
-                # print(self.dataa.elevation)
-                # print("jnvfkjvkjfvjsfvjJNJNEVJINVIEJNVIPJJRF")
-                print(f"self.unpacked_data.elevation is {self.unpacked_data.elevation}")
 
-                assert self.unpacked_data.command == 106
-                print(f"self.unpacked_data is : {self.unpacked_data}")
-                self.interlock_mode = self.unpacked_data.mode
                 self.interlock_elevation = self.unpacked_data.elevation
-                self.interlock_reserverd = self.unpacked_data.reserved
-
-                # self.interlock_parser()
-
-    def asf_parser(self, unpacked_data):
-        assert unpacked_data.command == 100
-        self.asf_mode = unpacked_data.mode
-        self.asf_offset_dr_nr = unpacked_data.offset_dr_nr
-        self.asf_offset_active = unpacked_data.offset_active
-        self.asf_offset_active1 = unpacked_data.offset_active1
-        self.asf_offset_active2 = unpacked_data.offset_active2
-        self.asf_offset_active3 = unpacked_data.offset_active3
-        self.asf_offset_active4 = unpacked_data.offset_active4
-        self.asf_offset_active5 = unpacked_data.offset_active5
-        self.asf_offset_active6 = unpacked_data.offset_active6
-        self.asf_offset_active7 = unpacked_data.offset_active7
-        self.asf_offset_active8 = unpacked_data.offset_active8
-        self.asf_offset_active9 = unpacked_data.offset_active9
-        self.asf_offset_active10 = unpacked_data.offset_active10
-        self.asf_offset_active11 = unpacked_data.offset_active11
-
-
-
-    def hexapod_parser(self, unpacked_data):
-        assert unpacked_data.command == 101
-        self.hexapod_mode = unpacked_data.mode
-        self.hexapod_mode_lin = unpacked_data.mode_lin
-        self.hexapod_anzahl_lin = unpacked_data.anzahl_lin
-        self.hexapod_phase_lin = unpacked_data.phase_lin
-        self.hexapod_p_xlin = unpacked_data.p_xlin
-        self.hexapod_p_ylin = unpacked_data.p_ylin
-        self.hexapod_p_zlin = unpacked_data.p_zlin
-        self.hexapod_p_zlin = unpacked_data.p_zlin
-        self.hexapod_v_cmd = unpacked_data.v_cmd
-        self.hexapod_mode_rot = unpacked_data.mode_rot
-        self.hexapod_anzahl_rot = unpacked_data.anzahl_rot
-        self.hexapod_phase_rot = unpacked_data.phase_rot
-        self.hexapod_p_xrot = unpacked_data.p_xrot
-        self.hexapod_p_yrot = unpacked_data.p_yrot
-        self.hexapod_p_zrot = unpacked_data.p_zrot
-        self.hexapod_v_rot = unpacked_data.v_rot
-
-
-    def polar_parser(self, unpacked_data):
-        assert unpacked_data.command == 102
-        self.interlock_mode = unpacked_data.mode
-        self.interlock_p_soll = unpacked_data.p_soll
-        self.interlock_v_cmd = unpacked_data.v_cmd
-
-    # def interlock_parser(self):
-    #     unpacked_data = self.unpacked_data
-
+                self.interlock_reserved = self.unpacked_data.reserved
+                self.interlock_mode = self.unpacked_data.mode
 
 
 
